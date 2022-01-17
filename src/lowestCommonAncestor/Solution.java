2a4e8cfed20dc050a3ff81fac0f3d00f1b98c922
miwukai <miwk@linkstec.com>
2022-01-18 00:02:44 +0800

    236.lowestCommonAncestor

---
+++ src/lowestCommonAncestor/Solution.java
@@ -0,0 +1,20 @@
+package lowestCommonAncestor;
+
+public class Solution {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+		
+		
+		return null;
+	}
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
