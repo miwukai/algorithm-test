d98d3bfc89a459bc88173422fa98505634c8cb03
miwukai <milkiy@yeah.net>
2022-01-18 00:31:50 +0800

    236.lowestCommonAncestor

--- src/lowestCommonAncestor/Solution.java
+++ src/lowestCommonAncestor/Solution.java
@@ -1,13 +1,34 @@
 package lowestCommonAncestor;
 
 public class Solution {
+	
+	TreeNode lca;
+	
 	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
+		return lowestCommonAncestorRec(root,p,q);
 		
-		
-		return null;
 	}
+	
+	public TreeNode lowestCommonAncestorRec(TreeNode root, TreeNode p, TreeNode q) {
 
+		if(lca != null) return null;
+		
+		if(root == null || root.equals(p) || root.equals(q)) return root;
+		
+		TreeNode left = lowestCommonAncestorRec(root.left, p, q);
+		
+		TreeNode right = lowestCommonAncestorRec(root.right, p, q);
+		
+		if(left != null && right != null) {
+			lca = root;
+			return lca;
+		}
+		
+		return left == null ? right : left;
+		
+	}
+	
 	public class TreeNode {
 		int val;
 		TreeNode left;
